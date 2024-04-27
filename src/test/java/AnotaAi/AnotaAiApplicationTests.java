ac065f957328be8a4db49fdfc580ed91e976410f
Greicy Itakura <greicyitakura@gmail.com>
2024-04-27 16:59:33 -0300

    project created

---
+++ src/test/java/AnotaAi/AnotaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package AnotaAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
